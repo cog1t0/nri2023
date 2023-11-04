2ee24fe62760c6621f22eec33aec534401716f25
cog1t0 <riku4724@gmail.com>
2023-11-04 17:45:56 +0900

    add test

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,8 +8,9 @@ class HomeController < ApplicationController
 
   def webhook
     Rails.logger.debug("===== webhook =====")
-    # body = request.body.read
-    # events = line_client.parse_events_from(body)
+    body = request.body.read
+    events = line_client.parse_events_from(body)
+    Rails.logger.debug("===== #{events} =====")
     # events.each do |event|
     #   @line_id = event['source']['userId']
     #   @user = User.find_or_create_by_line_id(@line_id)
