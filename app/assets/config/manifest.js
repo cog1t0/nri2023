dab8f5e8c37b33ed40984a110b5fafe856d9a960
hirokinakano <nkn12151018@gmail.com>
2023-11-05 02:48:46 +0900

    🎃 LINEログインの実装とフォームのデザインを調整

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,3 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
-/* 不要そうなのでコメントアウト
-//= link_tree ../../../vendor/javascript .js
-*/
--- config/initializers/line_bot.rb
+++ config/initializers/line_bot.rb
@@ -1,6 +1,6 @@
 require 'line/bot'
 
 LineBot = Line::Bot::Client.new { |config|
-  config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-  config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+  config.channel_secret = ENV["LINE_CHANNEL_SECRET"] || "e9d03632772716b6592130858b0f15de"
+  config.channel_token = ENV["LINE_CHANNEL_TOKEN"] || "n9Yh2Hzr4M+YGew2XO+yFO4mAEGGXlU3dsww+rVkBeJe5NHwl4HGv790ZJxDLx4q91VaRwR3G+x6BHWiFfD3SV0snRhE9vehLVddNuXlBEE8snXu4beM8Y8J3EC2XOegyg7y6yyvWmKJSro5PmDLsgdB04t89/1O/w1cDnyilFU="
 }
