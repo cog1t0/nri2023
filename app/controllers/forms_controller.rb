f30278bba486c1b79c0f25eb2282452b92234de5
hirokinakano <nkn12151018@gmail.com>
2023-11-04 18:43:22 +0900

    ✨ プロフィール登録画面作成

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -1,4 +1,37 @@
 class FormsController < ApplicationController
-  def profile_get
+  def line_login
+    @event_id = params[:event_id]
+    render 'forms/line_login'
+  end
+
+  def show
+    event_id = params[:event_id]
+
+    # イベントが見つからない場合、最初のイベントを取得する。本当はだめだよ!!
+    event = Event.find_by(id: event_id) || Event.first
+    # TODO: LINE連携のパラメーターをUserのモデルへセットする
+    @user = event.users.build(line_id: "line_id")
+  end
+
+  def create
+    @user = User.new(user_params)
+    # TODO: アイコンのURLをセットする
+    @user.icon_id = Icon.first.id
+    
+    if @user.save
+      redirect_to success_path
+    else
+      render 'forms/show'
+    end
+  end
+
+  def create_success
+    render 'forms/create_success'
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name1, :name2, :profile, :from, :to, :line_id, :event_id, :line_id)
   end
 end
--- test/controllers/forms_controller_test.rb
+++ test/controllers/forms_controller_test.rb
@@ -1,8 +1,13 @@
 require "test_helper"
 
 class FormsControllerTest < ActionDispatch::IntegrationTest
-  test "should get profile_get" do
-    get forms_profile_get_url
+  test "should get show" do
+    get forms_show_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get forms_create_url
     assert_response :success
   end
 end
