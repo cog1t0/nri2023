8bdef06b1fab71b69acf2b57b4474f94de4d82e5
cog1t0 <riku4724@gmail.com>
2023-11-04 15:57:42 +0900

    add test action

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,12 @@ class HomeController < ApplicationController
   def index
     render plain: 'Hello World!'
   end
+
+  def webhook
+    return :success
+  end
+
+  def line_bot_send_push_message
+    LineBot.push_message('LINEID', { type: 'text', text: 'Hello World!' })
+  end
 end
